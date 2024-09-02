aee9680725ea29140be753f080b6cade6b597f09
lincentpega <imkrasnyukov@gmail.com>
2024-09-02 22:01:51 +0500

    Add some files

---
+++ cmd/web/templates.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"html/template"
+	"path/filepath"
+	"time"
+
+	"github.com/lincentpega/personal-crm/internal/models"
+)
+
+type templateData struct {
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+	CurrentYear int
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := make(map[string]*template.Template)
+
+	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+        ts := template.New(name)
+        ts.Funcs(functions)
+		ts, err := ts.ParseFiles("./ui/html/base.html")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
